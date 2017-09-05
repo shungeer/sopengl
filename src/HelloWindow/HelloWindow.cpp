6968890697ad59bed6eb92bd977d0d24ca027947
xys <bian_hope@163.com>
2017-09-05 20:46:50 +0800

    function: create window

---
+++ src/HelloWindow/HelloWindow.cpp
@@ -0,0 +1,78 @@
+// HelloWindow.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+using namespace std;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	// 1.GLFW配置
+	glfwInit();										// 初始化GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);	// 设置OpenGL主版本号是3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);	// 设置OpenGL次版本号是3
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);	// 设置OpenGL使用核心模式
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);	// 这是mac osx系统确保上述设置起作用的代码
+	
+	// 2.创建窗口，设置主上下文
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);		// 窗口对象
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);		// 设置这个窗口的上下文是当前线程的主上下文
+
+	// 3.使用GLAD库获取OpenGL的函数正确地址
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// 4.设置OpenGL的视口
+	// 实际上也可以将视口的维度设置为比GLFW的维度小，这样子之后所有的OpenGL渲染将会在一个更小的窗口中显示，这样子的话我们也可以将一些其它元素显示在OpenGL视口之外。
+	glViewport(0, 0, 800, 600);		// 参数说明：左下角位置+宽度+高度
+
+	// 5.注册回调函数（更改视口、处理手柄输入、处理错误消息等）
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);	// 更改视口
+	glfwSetKeyCallback(window, key_callback);							// 键盘输入
+
+	// 6.循环渲染
+	while (!glfwWindowShouldClose(window))
+	{
+		// 渲染命令
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwPollEvents();		// 检查触发事件（键盘、鼠标）+更新窗口状态，并相应调用回调函数
+		glfwSwapBuffers(window);// 交换颜色缓冲
+	}
+
+	// 7.释放窗口相关资源
+	glfwTerminate();
+	return 0;
+}
+
+// 当用户改变窗口的大小的时候，视口也应该被调整
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+// 按键输入
+// 参数说明：key：按下的按键；action：表示按键动作（按下或释放）；mode：表示是否有Ctrl、Shift、Alt、Super等按钮的操作
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	// 当用户按下ESC键,我们设置window窗口的WindowShouldClose属性为true
+	// 关闭应用程序
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
